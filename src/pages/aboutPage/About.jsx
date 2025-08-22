62c681e55e56c0076fbe80d1db666503d67298d4
Efuwa Akyere <efuwayorke13@gmail.com>
2025-08-22 08:25:20 +0000

    Continued with the About Us Page

--- src/pages/aboutPage/About.jsx
+++ src/pages/aboutPage/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../homePage/components/Navbar'
+import Hero from './components/Hero'
+import Mission from './components/Mission'
+import WhatWeDo from './components/WhatWeDo'
+import Footer from '../homePage/components/Footer'
 
 const About = () => {
   return (
     <div>
         <Navbar />
+        <Hero />
+        <WhatWeDo />
+        <Mission />
+        <Footer />
     </div>
   )
 }
